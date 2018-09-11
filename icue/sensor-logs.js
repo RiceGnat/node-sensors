27e7b7542f1ca528a17f87e3e5773d1197c11ae1
RiceGnat <ricegnat@gmail.com>
2018-09-11 00:47:50 -0500

    more consistent error msgs

--- icue/sensor-logs.js
+++ icue/sensor-logs.js
@@ -11,10 +11,10 @@ const getSensorLogFolder = () => {
         fs.readFile(path.join(appdata, "Corsair/CUE/config.cuecfg"), 'utf8', (err, data) => {
             if (err) return reject(err);
             xml.parseString(data, (err, result) => {
-                if (err) return reject("Could not locate iCUE configuration");
+                if (err) return reject("Couldn't locate iCUE configuration.");
                 const sensorLogging = result.config.map.filter(map => map.$.name === "SensorLogging")[0];
                 if (sensorLogging) resolve(sensorLogging.value.filter(value => value.$.name === "Folder")[0]._);
-                else reject("Sensor logging not configured");
+                else reject("Sensor logging not configured.");
             })
         });
     });
@@ -23,9 +23,9 @@ const getSensorLogFolder = () => {
 const getLatestLogFile = dir => {
     return new Promise((resolve, reject) => {
         fs.readdir(dir, (err, files) => {
-            if (err) return reject("Sensor log directory not found or could not be read");
+            if (err) return reject("Sensor log directory not found or couldn't be read.");
             files = files.filter(file => file.startsWith(filenameBase)).sort();
-            if (files.length === 0) return reject("No sensor logs found");
+            if (files.length === 0) return reject("No sensor logs found.");
             resolve(path.join(dir, files[files.length - 1]));
         })
     });
@@ -34,7 +34,7 @@ const getLatestLogFile = dir => {
 const getLatestSensorValues = file => {
     return new Promise((resolve, reject) => {
         fs.readFile(file, 'utf8', (err, data) => {
-            if (err) return reject("Could not read sensor log file");
+            if (err) return reject("Couldn't read sensor log file.");
             const values = csv(data, { columns: true });
             resolve(Object.entries(values[values.length - 1])
                 .filter(([key, value]) => key !== "Timestamp")
